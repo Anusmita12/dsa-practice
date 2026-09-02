cb68987164e11356ea824a79b458b60f43530780
Anusmita12 <anushmita5@icloud.com>
2026-09-02 22:52:14 +0530

    Add Two Sum optimal solution

---
+++ 01_two_sum_optimal.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSumOptimized(vector<int>& nums, int target) {
+    unordered_map<int, int> seen; 
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (seen.find(complement) != seen.end()) {
+            return {seen[complement], i};
+        }
+
+        seen[nums[i]] = i; 
+    }
+    return {};
+}
+int main()
+{
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> result = twoSumOptimized(nums,target);
+    cout << "Indices: "<<result[0]<<","<<result[1]<< endl;
+    return 0;
+}
